2ac061e78df86021edbd6102b2c82a772a4a03a5
Chris Magnussen <chris@hirvi.no>
2017-05-03 18:32:40 +0200

    Update config

--- src/Laravel/config/ticketco.php
+++ src/Laravel/config/ticketco.php
@@ -5,7 +5,8 @@ return [
     | TicketCo API key
     |--------------------------------------------------------------------------
     |
-    | To obtain an API key, contact TicketCo.
+    | To obtain an API key, contact TicketCo or fill out this form:
+    | https://app.pipefy.com/public_form/155824
     |
     */
     'apikey' => ''
